6267dafe80781fc54bf64f7c90704e2887e4800a
Chaeguevara <dfso2@naver.com>
2022-02-24 21:44:47 +0900

    Lecture 1 done & lecture 2 going

---
+++ lec2/example.sh
@@ -0,0 +1,13 @@
+
+echo "Starting program at $(date)"
+
+echo "Running program $0 with $# arguments with pid $$"
+
+for file in "$@"; do
+	grep foobar "$file" > /dev/null 2> /dev/null
+	if[["$?" -ne 0]]; then
+		echo "File $file does not have any foobar, ading one"
+		echo "# foobar" >> "$file"
+	fi
+done
+
